a8e34b40df33505505098ae96ea5584e553ee2df
Robert Huang <56649783@qq.com>
2024-05-16 10:31:25 +0800

    rename deps install scripts filename

--- deps.macos-12+.sh
+++ deps.macos-12+.sh
@@ -3,4 +3,4 @@
 # This script is used to install dependencies on MacOS 12.0 or newer
 # botan v3
 brew update
-brew install googletest spdlog c-ares openssl@3  botan
+brew install googletest spdlog c-ares openssl@3 botan
