b2101c915a5a6a9b51232eb1a16f37bc4d4af9be
Robert Huang <56649783@qq.com>
2024-08-18 22:21:51 +0800

    update

--- deps.rockylinux-9+.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# This script is used to install dependencies on RockyLinux 9 or newer
-dnf install epel-release -y
-dnf update
-dnf install gtest-devel spdlog-devel c-ares-devel openssl-devel -y
--- deps.ubuntu-20.04+.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# This script is used to install dependencies on Ubuntu 20.04 or newer
-
-# Installing packages might fail as the github image becomes outdated
-sudo apt update
-sudo apt install curl libgtest-dev libspdlog-dev libc-ares-dev openssl libssl-dev -y
